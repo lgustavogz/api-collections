fc3e1915e5211657145757036366607ff671e6d5
Gustavo <luizgustavogz@hotmail.com>
2022-07-13 05:56:28 -0300

    Removed deprecated items

--- src/main/kotlin/grassi/gustavo/collections/TesteOperacoes.kt
+++ src/main/kotlin/grassi/gustavo/collections/TesteOperacoes.kt
@@ -8,11 +8,22 @@ fun main() {
     }
 
     println("__________________________")
-    println("Maior salario: ${salarios.max()}")
-    println("Menor salario: ${salarios.min()}")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
     println("Media salarial: ${salarios.average()}")
 
     val salarioMaiorQue2500 = salarios.filter { it > 2500.0 }
     println("__________________________")
     salarioMaiorQue2500.forEach { println(it) }
+
+    println("__________________________")
+    println(salarios.count { it in 1000.0..5000.9 })
+
+    println("__________________________")
+    println(salarios.find { it == 3000.3 })
+    println(salarios.find { it == 3456.9 })
+
+    println("__________________________")
+    println(salarios.any { it == 10234.423 })
+    println(salarios.any { it == 1000.1 })
 }
